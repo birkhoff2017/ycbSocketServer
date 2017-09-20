423138a74f20085405d72c62a1b82dc463b3985f
朱辉 <pzzhuhui@gmail.com>
2017-09-20 19:25:25 +0800

    add client init

---
+++ src/test/java/ClientInitializer.java
@@ -0,0 +1,32 @@
+import com.ycb.socket.dispatcher.HandlerDispatcher;
+import com.ycb.socket.netty.ServerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class ClientInitializer extends ChannelInitializer<SocketChannel> {
+
+    private HandlerDispatcher handlerDispatcher;
+
+    public void init() {
+        new Thread(this.handlerDispatcher).start();
+    }
+
+    public void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        // 以("\n")为结尾分割的 解码器
+        pipeline.addLast("framer", new DelimiterBasedFrameDecoder(Integer.MAX_VALUE, Delimiters.lineDelimiter()));
+        // 字符串解码 和 编码
+        pipeline.addLast("encoder", new StringEncoder());
+        pipeline.addLast("decoder", new StringDecoder());
+        pipeline.addLast("handler", new ServerAdapter(this.handlerDispatcher));
+    }
+
+    public void setHandlerDispatcher(HandlerDispatcher handlerDispatcher) {
+        this.handlerDispatcher = handlerDispatcher;
+    }
+}
